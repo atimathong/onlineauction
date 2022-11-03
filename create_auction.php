ca047a9fdb710b1ab2b31c603389f268568f612d
Judy <c20chin@gmail.com>
2022-11-03 10:57:57 +0000

    button

---
+++ create_auction.php
@@ -0,0 +1,156 @@
+<?php 
+include_once 'database_connect/connect_db.php';
+include_once("top_header.php");
+
+?>
+
+
+<?php
+/* (Uncomment this block to redirect people without selling privileges away from this page)
+  // If user is not logged in or not a seller, they should not be able to
+  // use this page.
+  if (!isset($_SESSION['account_type']) || $_SESSION['account_type'] != 'seller') {
+    header('Location: browse.php');
+  }
+*/
+?>
+
+<div class="container">
+
+<!-- Create auction form -->
+<div style="max-width: 800px; margin: 10px auto">
+  <h2 class="my-3">Create new auction</h2>
+  <div class="card">
+    <div class="card-body">
+      <!-- Note: This form does not do any dynamic / client-side / 
+      JavaScript-based validation of data. It only performs checking after 
+      the form has been submitted, and only allows users to try once. You 
+      can make this fancier using JavaScript to alert users of invalid data
+      before they try to send it, but that kind of functionality should be
+      extremely low-priority / only done after all database functions are
+      complete. -->
+      <form method="post" action="create_auction_result.php">
+        <div class="form-group row">
+          <label for="item_name" class="col-sm-2 col-form-label text-right">Title of auction</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="auctionTitle" name="item_name" placeholder="e.g. Black mountain bike">
+            <small id="titleHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> A short description of the item you're selling, which will display in listings.</small>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="description" class="col-sm-2 col-form-label text-right">Details</label>
+          <div class="col-sm-10">
+            <textarea class="form-control" id="auctionDetails" name="description" rows="4"></textarea>
+            <small id="detailsHelp" class="form-text text-muted">Full details of the listing to help bidders decide if it's what they're looking for.</small>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="condition" class="col-sm-2 col-form-label text-right">Condition</label>
+          <div class="col-sm-10">
+            <select class="form-control" id="auctionCategory" name="condition">
+              <option selected>Choose...</option> 
+              <option value="brandnew">Brand New</option>
+              <option value="likenew">Like New</option>
+              <option value="lightused">Lightly Used</option>
+              <option value="wellused">Well Used</option>
+              <option value="heavilyused">Heavily Used</option>
+              </select>
+            <small id="conditionHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> State the condition for this item.</small>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="category_ID" class="col-sm-2 col-form-label text-right">Category</label>
+          <div class="col-sm-10">
+            <select class="form-control" id="auctionCategory" name="category_ID">
+              <option selected>Choose...</option>
+              <option value="1">Body and Hair</option>
+              <option value="2">Sports</option>
+              <option value="3">Phones</option>
+              <option value="4">Books</option>
+              <option value="5">Arts</option>
+              <option value="6">Toys</option>
+              <option value="7">Cameras</option>
+              <option value="8">Jewellery and Watch</option>
+              <option value="9">Music Instruments</option>
+              <option value="10">Cars</option>
+              <option value="11">Laptops</option>
+              <option value="12">Furniture</option>
+            </select>
+            <small id="categoryHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Select a category for this item.</small>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="starting_price" class="col-sm-2 col-form-label text-right">Starting price</label>
+          <div class="col-sm-10">
+	        <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" >£</span>
+              </div>
+              <input type="number" name="starting_price" class="form-control" id="auctionStartPrice">
+            </div>
+            <small id="startBidHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Initial bid amount.</small>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="reserve_price" class="col-sm-2 col-form-label text-right">Reserve price</label>
+          <div class="col-sm-10">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" name="reserve_price">£</span>
+              </div>
+              <input type="number" class="form-control" name="reserve_price" id="auctionReservePrice">
+            </div>
+            <small id="reservePriceHelp" class="form-text text-muted">Optional. Auctions that end below this price will not go through. This value is not displayed in the auction listing.</small>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="start_date" class="col-sm-2 col-form-label text-right">Start Date</label>
+          <div class="col-sm-10">
+            <input type="date" class="form-control" id="auctionEndDate" name="start_date">
+            <small id="startDateHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Day for the auction to start.</small>
+          </div>
+        </div>      
+        <div class="form-group row">
+          <label for="start_time" class="col-sm-2 col-form-label text-right">Start Time</label>
+          <div class="col-sm-10">
+            <input type="time" class="form-control" id="auctionEndDate" name="start_time">
+            <small id="startTimeHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+          </div>
+        </div> 
+        <div class="form-group row">
+          <label for="end_date" class="col-sm-2 col-form-label text-right">End Date</label>
+          <div class="col-sm-10">
+            <input type="date" class="form-control" id="auctionEndDate" name="end_date">
+            <small id="endDateHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Day for the auction to end.</small>
+          </div>
+        </div>  
+        <div class="form-group row">
+          <label for="end_time" class="col-sm-2 col-form-label text-right">End Time</label>
+          <div class="col-sm-10">
+            <input type="time" class="form-control" id="auctionEndDate" name="end_time">
+            <small id="endTimeHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+          </div>
+        </div> 
+        <div class="form-group row">
+          <label for="picture" class="col-sm-2 col-form-label text-right">Add Picture</label>
+          <div class="col-sm-10">
+            
+            <!-- <form action="upload.php" method="post" enctype="multipart/form-data">
+                Select image to upload:
+              <input type="file"  id="fileToUpload">
+              <input type="submit" name="photosubmit" value="Upload Image" >
+              <small id="pictureHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Upload only .jpg or .jpeg files.</small>
+            </form> -->
+            
+          </div>
+        </div>
+        <button type="submit" name="new_auction" class="btn btn-primary form-control">Create Auction</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+<?php include_once("bottom_footer.php")?>
---
+++ upload.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+include_once 'database_connect/connect_db.php';
+$id = $_SESSION['userid']; 
+
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["photosubmit"])) {
+  $fileName = $_FILES['file']['name'];
+  $fileTmpName = $_FILES['file']['tmp_name'];
+  $fileSize = $_FILES['file']['size'];
+  $fileError = $_FILES['file']['error'];
+  $fileType = $_FILES['file']['type'];
+  $picid = uniqid('',true);
+
+  $fileExt = explode(',', $fileName);
+  $fileActualExt = strtolower(end($fileExt));
+
+  if (in_array($fileActualExt, $allowed)){
+    if ($fileError === 0) {
+        if ($fileSize < 1000000){
+          $fileNameNew = "product".$id.$picid.".".$fileActualExt;
+          $fileDestination = 'uploads/'.$fileNameNew;
+          move_uploaded_file($fileTmpName, $fileDestination);
+          $sql = "UPDATE item SET picture='$fileNameNew' WHERE userid='$id';";
+          $result = mysqli_query($db_conn, $sql);
+
+          header("Location: index.php?uploadsuccess");
+        } else {
+          echo "Your file is too large.";
+        }
+    } else {
+      echo "An error occured.";
+    }
+  } else {
+    echo "Please only upload .jpg/.jpeg/.png files.";
+  }
+
+  $allowed = array('jpg', 'jpeg', 'png');
+
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
