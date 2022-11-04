a61afb45c40388259fa4995e011b6f9414298cc9
Judy <c20chin@gmail.com>
2022-11-04 10:31:23 +0000

    failed

--- create_auction.js
+++ create_auction.js
@@ -9,17 +9,19 @@ const auctionStartDate = document.getElementById('auctionStartDate');
 const auctionStartTime = document.getElementById('auctionStartTime');
 const auctionEndDate = document.getElementById('auctionEndDate');
 const auctionEndTime = document.getElementById('auctionEndTime');
-const auctionFile = document.getElementById('file');
+const isValid=False;
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
-	
 	checkInputs();
+	alert(isValid);
+	if (isValid==False){
+		e.preventDefault();
+	}
 });
 
 function checkInputs() {
 	// trim to remove the whitespaces
-	const auctionFileValue = auctionFile.value;
 	const auctionTitleValue = auctionTitle.value.trim();
 	const auctionDetailsValue = auctionDetails.value.trim();
 	const auctionCategoryValue = auctionCategory.value;
@@ -30,74 +32,92 @@ function checkInputs() {
 	const auctionEndTimeValue =auctionEndTime.value.trim();
 	const auctionStartPriceValue = auctionStartPrice.value.trim();
 	const auctionReservePriceValue =auctionReservePrice.value;
+	const isValid=False;
 
-	if(auctionFileValue === '') {
-		setErrorFor(auctionFile, 'Please upload a file');
-	} else {
-		setSuccessFor(auctionFile);
-	}
+	
+	
 
 	if(auctionTitleValue === '') {
 		setErrorFor(auctionTitle, 'Title cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionTitle);
+		isValid=True;
 	}
 	
 	if(auctionDetailsValue === '') {
 		setErrorFor(auctionDetails, 'Details cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionDetails);
+		isValid=True;
 	}
 	
-	if(auctionCategoryValue) {
+	if(auctionCategoryValue === 'none') {
 		setErrorFor(auctionCategory, 'Field cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionCategory);
+		isValid=True;
 	}
 
-	if(auctionConditionValue) {
+	if(auctionConditionValue ==="none") {
 		setErrorFor(auctionCondition, 'Field cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionCondition);
+		isValid=True;
 	}
 
 	if(auctionStartDateValue === '') {
 		setErrorFor(auctionStartDate, 'Field cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionStartDate);
+		isValid=True;
 	}
 
 	if(auctionStartTimeValue === '') {
 		setErrorFor(auctionStartTime, 'Field cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionStartTime);
+		isValid=True;
 	}
 
 	if(auctionEndDateValue === '') {
 		setErrorFor(auctionEndDate, 'Field cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionEndDate);
+		isValid=True;
 	}
 
 	if(auctionEndTimeValue === '') {
 		setErrorFor(auctionEndTime, 'Field cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionEndTime);
+		isValid=True;
 	}
 
 	if(auctionStartPriceValue === '') {
 		setErrorFor(auctionStartPrice, 'Field cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionStartPrice);
+		isValid=True;
 	}
 
 	if(auctionReservePriceValue === '') {
 		setErrorFor(auctionReservePrice, 'Field cannot be blank');
+		isValid=False;
 	} else {
 		setSuccessFor(auctionReservePrice);
+		isValid=True;
 	}
 
-
+	
 	
 }
 
@@ -112,4 +132,40 @@ function setSuccessFor(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-con success';
 }
+
+
+
 	
+function fileValidation() {
+	var fileInput =
+	  document.getElementById('file');
+	
+	var filePath = fileInput.value;
+  
+	// Allowing file type
+	var allowedExtensions =
+		/(\.jpg|\.jpeg|\.png)$/i;
+	
+	if (!allowedExtensions.exec(filePath)) {
+	  alert('Invalid File Type');
+	  fileInput.value = '';
+	  return false;
+	}
+	else
+	{
+	
+	  // Image preview
+	  if (fileInput.files && fileInput.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+		  document.getElementById(
+			'imagePreview').innerHTML =
+			'<img src="' + e.target.result
+			+ '"/ height = 80 width = 80>';
+		};
+		
+		reader.readAsDataURL(fileInput.files[0]);
+	  }
+	}
+}	
+
--- create_auction_result.php
+++ create_auction_result.php
@@ -5,6 +5,8 @@ $id = $_SESSION['userid'];
 $itemid = $_SESSION['itemid'];
 ?>
 
+
+
 <div class="container my-5">
 
 <?php
@@ -31,35 +33,6 @@ if (isset($_POST['new_auction'])) {
     $end_time = $_POST['end_time'];
 
    
-
-    //form validation
-
-    /* 
-    if (empty($item_name)) {
-        array_push($errors, "First name is required");
-    };
-    if (empty($lastname)) {
-        array_push($errors, "Last name is required");
-    };
-    if (empty($password_1)) {
-        array_push($errors, "Password is required");
-    };
-    if ($password_1 != $password_2) {
-        array_push($errors, "Passwords do not match");
-    }
-    if (empty($addressline_1)) {
-        array_push($errors, "Address is required");
-    };
-    if (empty($postal_code)) {
-        array_push($errors, "Postal code is required");
-    };
-    if (empty($phone_number)) {
-        array_push($errors, "Phone number is required");
-    };
-    if (empty($user_type)) {
-        array_push($errors, "User type is required");
-    }; */
-
     
     
 
@@ -68,41 +41,39 @@ if (isset($_POST['new_auction'])) {
 
 /* TODO #3: If everything looks good, make the appropriate call to insert
             data into the database. */
-    if (count($errors) === 0) {
-        $query = "UPDATE item 
-        SET 
-        item_name= '$item_name', 
-        pro_desc= '$description',
-        cond= '$condition',
-        starting_price='$starting_price',
-        reserve_price='$reserve_price',
-        sta_date='$start_date',
-        start_time='$start_time',
-        end_date='$end_date',
-        end_time='$end_time',
-        category_ID='$category_ID'
-        WHERE item_ID='$itemid';";
-        
-        // echo "<pre>Debug: $query</pre>\m";
-        $result = mysqli_query($db_conn, $query);
-        // echo $result;
-        /* FOR DEBUGGING
-        if ( false===$result ) {
-            printf("error: %s\n", mysqli_error($db_conn));
-          }
-          else {
-            echo 'done.';
-          }
-        */
-        
+if (count($errors) === 0) {
+    $query = "UPDATE item 
+    SET 
+    item_name= '$item_name', 
+    pro_desc= '$description',
+    cond= '$condition',
+    starting_price='$starting_price',
+    reserve_price='$reserve_price',
+    sta_date='$start_date',
+    start_time='$start_time',
+    end_date='$end_date',
+    end_time='$end_time',
+    category_ID='$category_ID'
+    WHERE item_ID='$itemid';";
+    
+    echo "<pre>Debug: $query</pre>\m";
+    $result = mysqli_query($db_conn, $query);
+    
+    echo $result;
+    //FOR DEBUGGING
+    if ( false===$result ) {
+        printf("error: %s\n", mysqli_error($db_conn));
+        }
+        else {
+        echo 'done.';
+        }
 
-        //header('refresh:5;url=index.php');
-         }        
+    
+    //header('refresh:5;url=index.php');
+        }        
 
 // If all is successful, let user know.
 echo('<div class="text-center">Auction successfully created! <a href="index.php">View your new listing.</a></div>');
-
-
 ?>
 
 </div>
