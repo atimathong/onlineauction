020f9344c63cb3442ec4186b54a40687b0354403
peteryu6688 <115811141+peteryu6688@users.noreply.github.com>
2022-11-10 12:26:58 +0000

    #25 Added the delete item button

--- watchlist.php
+++ watchlist.php
@@ -12,6 +12,11 @@ table, th, td {
   border:1px solid black;
 }
 </style>
+<head>
+<title>Font Awesome Icons</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
 <body>
 
 
@@ -48,7 +53,7 @@ $fname =  $_SESSION['fname'];
 
 echo "<h1> Welcome $fname. This is your watchlist </h1>";
 
-$query1 = "SELECT item_name, pro_desc, picture FROM item WHERE item_ID IN (SELECT item_ID 
+$query1 = "SELECT item_ID, item_name, pro_desc, picture FROM item WHERE item_ID IN (SELECT item_ID 
 FROM watchlist WHERE user_ID = '$user_id')";
 $result1= mysqli_query($db_conn, $query1);
 
@@ -58,6 +63,8 @@ $result1= mysqli_query($db_conn, $query1);
     <th>Item Name </th>
     <th>Item Description </th>
     <th> Item Picture </th>
+    <th> Product Page </th>
+    <th> Delete Item </th>
 </tr>
 <?php
 while ($row = mysqli_fetch_assoc($result1)) {
@@ -66,6 +73,11 @@ while ($row = mysqli_fetch_assoc($result1)) {
      <td> <?php echo $row['item_name'] ?> </td>
      <td> <?php echo $row['pro_desc'] ?> </td>
      <td> <img src="pictures/<?php echo $row['picture']; ?>" class="card-img-top" alt="product" style="width:380px;height:280px;"> </td>
+     <td>  <a href="product_details.php?id=<?php echo $row['item_ID']; ?>"> Link </a> </td>
+     <td> <form action = 'delete.php' method = 'post' target="_self">
+          <button style="font-size:24px" type="submit" name="delete"> <i style="font-size:24px" class="fa">&#xf014;</i> </td>
+          <input type = "hidden" name = "item_ID" value = "<?=$row['item_ID']?>">
+          </form> 
     </tr> 
 <?php }   
 ?>
