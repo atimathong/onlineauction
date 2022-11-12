d5d9f2ef8bd5dd79bdb9168c4d0a7dd78d930a44
peteryu6688 <115811141+peteryu6688@users.noreply.github.com>
2022-11-12 12:34:12 +0000

    #25 Added the recommendation feature

--- recommendations.php
+++ recommendations.php
@@ -1,5 +1,10 @@
 <?php include_once("top_header.php")?>
 <?php require("utilities.php")?>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
 
 <div class="container">
 
@@ -14,8 +19,31 @@
   
   
   // TODO: Check user's credentials (cookie/session).
+  $user_id = mysqli_real_escape_string($db_conn, $_SESSION['userid']);
   
   // TODO: Perform a query to pull up auctions they might be interested in.
+  $query = "SELECT item_ID, item_name, pro_desc, picture FROM item WHERE category_ID IN (SELECT category_ID FROM item WHERE item_ID IN (SELECT item_ID FROM view_history WHERE (item_ID IN (SELECT item_ID FROM item WHERE view_times = (SELECT MAX(view_times) FROM view_history)) AND user_ID = '$user_id')))";
+  $query_result = mysqli_query($db_conn, $query);
+  ?>
+  <table style = "width:100%">
+  <tr>
+    <th>Item Name </th>
+    <th>Item Description </th>
+    <th> Item Picture </th>
+    <th> Product Page </th>
+  </tr>
+  <?php
+  while($row = mysqli_fetch_assoc($query_result)){
+    ?>
+    <tr>
+    <td> <?php echo $row['item_name']; ?> </td>
+    <td> <?php echo $row['pro_desc'] ?> </td>
+    <td> <img src="pictures/<?php echo $row['picture']; ?>" class="card-img-top" alt="product" style="width:380px;height:280px;"> </td>
+    <td>  <a href="product_details.php?id=<?php echo $row['item_ID']; ?>"> Link </a> </td>
+    </tr>
+  <?php
+  }
+  
   
   // TODO: Loop through results and print them out as list items.
   
