ce633c737e46a414bd2203d7a68cc3ee337bdb09
Judy <c20chin@gmail.com>
2022-12-10 23:33:00 +0000

    bug-fixed

--- create_auction.js
+++ create_auction.js
@@ -156,37 +156,3 @@ function setSuccessFor(input) {
 
 
 
-	
-function fileValidation() {
-	var fileInput =
-	  document.getElementById('file');
-	
-	var filePath = fileInput.value;
-  
-	// Allowing file type
-	var allowedExtensions =
-		/(\.jpg|\.jpeg|\.png)$/i;
-	
-	if (!allowedExtensions.exec(filePath)) {
-	  alert('Invalid File Type.\nUpload only .jpg/.jpeg/.png files.');
-	  fileInput.value = '';
-	  return false;
-	}
-	else
-	{
-	
-	  // Image preview
-	  if (fileInput.files && fileInput.files[0]) {
-		var reader = new FileReader();
-		reader.onload = function(e) {
-		  document.getElementById(
-			'imagePreview').innerHTML =
-			'<img src="' + e.target.result
-			+ '"/ height = 80 width = 80>';
-		};
-		
-		reader.readAsDataURL(fileInput.files[0]);
-	  }
-	}
-}	
-
--- create_auction_1.php
+++ create_auction_1.php
@@ -27,7 +27,7 @@ include_once("seller_header.php");
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
-    <script defer src="./create_auction.js"></script>
+    <script defer src="./img_upload.js"></script>
     <script>
     </script>
 </head>
@@ -44,26 +44,26 @@ include_once("seller_header.php");
     <small id="formHelp" class="form-text text-muted"><span class="text-danger">* Fields are Required.</span>
           
       <br>
-      
         
-      <form action="img_upload.php" method="POST" id= "img-upload" enctype="multipart/form-data">
+      <form action="img_upload.php" method="POST" id= "img-upload" onsubmit="return uploadValidation()" enctype="multipart/form-data">
         <div class="form-group form-con row">
-        <label for="picture" class="col-sm-2 col-form-label text-right">Add Picture<span class="text-danger">  *</span></label>
-        <div class="col-sm-10 form-con-btn">
-            Select image to upload:<br>
-          <input type="file" name="file" id="file" onchange="return fileValidation()"><br>
-          <div id="imagePreview"></div><br>
-          <button type="submit" name="img_submit" >UPLOAD</button>
-          <p id="pictureHelp" class="form-text text-muted"> Upload only .jpg/.jpeg/.png files.</p>
-          
-          <?php
-          if (isset($_FILES['file'])) {
-            echo '<script type=text/javascript>alert("success");</script>';
-            echo '<a target="_blank"><img src="pictures/'.$_SESSION['img'].'" height = 80 width = 80 class="img-fluid img-thumbnail " alt="product"></a>';
-          }
-          ?>
-          <small>Upload Success!</small>
-        </div>
+          <label for="picture" class="col-sm-2 col-form-label text-right">Add Picture<span class="text-danger">  *</span></label>
+          <div class="col-sm-10 form-con-btn">
+              Select image to upload:<br>
+            <input type="file" name="file" id="file" onchange="return fileValidation()"><br>
+            <div id="imagePreview"></div><br>
+            <button type="submit" name="img_submit">UPLOAD</button>
+            <p id="pictureHelp" class="form-text text-muted"> Upload only .jpg/.jpeg/.png files.</p>
+            
+            <?php
+            if (isset($_FILES['file']) && $_FILES['file'] !== "") {
+              echo '<script type=text/javascript>alert("success");</script>';
+              echo '<a target="_blank"><img src="pictures/'.$_SESSION['img'].'" height = 80 width = 80 class="img-fluid img-thumbnail " alt="product"></a>';
+              echo '<small>Upload Success!</small>';
+            } 
+            ?>
+            
+          </div>
         </div>
       </form>
       <br>
